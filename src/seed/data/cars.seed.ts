d9cd0d2e16e43212192169d1de5970cee3f4e73d
brunocoronado49 <francorangelcoronado@gmail.com>
2026-05-28 10:30:20 -0600

    CRUD brands, and seed config initial

---
+++ src/seed/data/cars.seed.ts
@@ -0,0 +1,25 @@
+import { v4 as uuid } from 'uuid';
+import { Car } from 'src/cars/interfaces/cars.interface';
+
+export const CARS_SEED: Car[] = [
+  {
+    id: uuid(),
+    brand: 'Toyota',
+    model: 'Corolla',
+  },
+  {
+    id: uuid(),
+    brand: 'Honda',
+    model: 'Civic',
+  },
+  {
+    id: uuid(),
+    brand: 'Nissan',
+    model: 'Tsuru',
+  },
+  {
+    id: uuid(),
+    brand: 'Jeep',
+    model: 'Cherookee',
+  },
+];
